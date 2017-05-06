4b779512ff794db04b92a774e3efd2a66a8f7788
Yogesh Kumar Arora <yogeshkumararora@users.noreply.github.com>
2017-05-06 17:04:02 +0530

    Added dummy code to increase LOC for SonarQube

--- src/main/java/experiments/home/HelloWorld.java
+++ src/main/java/experiments/home/HelloWorld.java
@@ -4,6 +4,14 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World GitHub - Master Updated Again!");
+		
+		String str = "Hello World";
+	      	String anotherString = "hello world";
+	      	Object objStr = str;
+
+   	       System.out.println( str.compareTo(anotherString) );
+	       System.out.println( str.compareToIgnoreCase(anotherString) );
+	       System.out.println( str.compareTo(objStr.toString()));
 	}
 
 }
