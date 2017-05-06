9cad8ed4fa8e8c4b19119880e5cca501c1551e33
Yogesh Kumar Arora <yogeshkumararora@users.noreply.github.com>
2017-05-06 17:06:57 +0530

    Introduced duplicate code

--- src/main/java/experiments/home/HelloWorld.java
+++ src/main/java/experiments/home/HelloWorld.java
@@ -13,5 +13,17 @@ public class HelloWorld {
 	       System.out.println( str.compareToIgnoreCase(anotherString) );
 	       System.out.println( str.compareTo(objStr.toString()));
 	}
+	
+	public void duplicateCodeExample() {
+		System.out.println("Hello World GitHub - Master Updated Again!");
+		
+		String str = "Hello World";
+	      	String anotherString = "hello world";
+	      	Object objStr = str;
+
+   	       System.out.println( str.compareTo(anotherString) );
+	       System.out.println( str.compareToIgnoreCase(anotherString) );
+	       System.out.println( str.compareTo(objStr.toString()));
+	}
 
 }
