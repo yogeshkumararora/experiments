f7a0c4e6dd2aa30f43e65154b693cfb2d8d5e152
Yogesh Kumar Arora <yogeshkumararora@users.noreply.github.com>
2017-05-06 17:24:15 +0530

    Introduced logging

--- src/main/java/experiments/home/HelloWorld.java
+++ src/main/java/experiments/home/HelloWorld.java
@@ -1,9 +1,14 @@
 package experiments.home;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloWorld {
 	
+        private final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World GitHub - Master Updated Again!");
+		logger.debug("Hello World GitHub - Master Updated Again!");
 		
 		String str = "Hello World";
 	      	String anotherString = "hello world";
